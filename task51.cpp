02fcfec4492bc64add2d857e8b0f9da221abbb86
DoSSi4 <57913839+DoSSi4@users.noreply.github.com>
2019-11-19 16:14:48 +0600

    Add files via upload

---
+++ task51.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	double a, b, c;
+	cin << a << b << c;
+	if (a + b == c || a + c == b || b + c == a) {
+		cout << "yes";
+
+	}
+	else {
+		cout << "no";
+	}
+	return 0;
+}
